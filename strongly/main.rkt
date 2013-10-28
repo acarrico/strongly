5993080da3d83564d8ca0b3445826a7c5f2c6d79
Anthony Carrico <acarrico@memebeam.org>
2013-10-27 22:27:05 -0400

    initial imperative version.

--- strongly/main.rkt
+++ strongly/main.rkt
@@ -11,83 +11,72 @@
 (require racket/generic)
 (require racket/match)
 
-(provide gen:sccable scc initial-scc-state scc-state-component-table)
+(provide gen:sccable scc initial-scc-state)
 
 ;;; Interface:
 
 (define-generics sccable
-  (sccable-fold-edges fn state sccable))
+  (sccable-index sccable)
+  (set-sccable-index! sccable index)
+  (sccable-fold-edges fn state sccable)
+  (sccable-component sccable)
+  (set-sccable-component! sccable component))
 
 (define (scc node (state (initial-scc-state)))
   (dfs node state))
 
-(struct scc-state
-  (index ; seen-so-far-index, aka preorder number
-   index-table ; node->index
-   S ; not-yet-assigned-to-an-scc
-   P ; not-yet-determined-to-belong-to-different-sccs
-   component-table ; node->component
-   ))
+(define (initial-scc-state) (scca-state 0 '() '()))
 
-(define (initial-scc-state)
-  (scc-state 0 (hasheq) '() '() (hasheq)))
-
-(define (sccable-component sccable state)
-  (hash-ref (scc-state-component-table state) sccable (lambda () #f)))
-
-(define (sccable-index sccable state)
-  (hash-ref (scc-state-index-table state) sccable (lambda () #f)))
+(struct scca-state (index ; seen-so-far-index, aka preorder number
+                    S ; not-yet-assigned-to-an-scc
+                    P ; not-yet-determined-to-belong-to-different-sccs
+                    ))
 
 ;;; Algorithm:
 
+;; should probably move the seen? test up to the entry point of the
+;; recursion.
+
 (define (dfs node state)
   ;; dfs = depth first search part of the algorithm.
-  (cond ((not (sccable-index node state))
-         ;; first time we see a node:
-         (maybe-pop-component
-          node
-          (sccable-fold-edges
-           dfs
-           (assign-index-and-push-stacks node state)
-           node)))
-        ((not (sccable-component node state))
-         ;; strongly connected:
-         (struct-copy
-          scc-state
-          state
-          (P (let ((i (sccable-index node state)))
-               (dropf (scc-state-P state)
-                      (lambda (n) (> (sccable-index n state) i)))))))
-        (else
-         ;; this part of the graph is already done:
-         state)))
+  (maybe-pop-component
+   node
+   (check-edges
+    node
+    (assign-index-and-push-stacks
+     node
+     state))))
 
-(define (assign-index-and-push-stacks node state)
-  (match state
-    ((struct* scc-state ((index i) (index-table index-table) (S S) (P P)))
-     (struct-copy
-      scc-state
-      state
-      (index (+ i 1))
-      (index-table (hash-set index-table node i))
-      (S (cons node S))
-      (P (cons node P))))))
+(define/match (assign-index-and-push-stacks node state)
+  ((node (struct* scca-state ((index index) (S S) (P P))))
+   (set-sccable-index! node index)
+   (scca-state (+ 1 index) (cons node S) (cons node P))))
+
+(define (check-edges node state)
+  (sccable-fold-edges
+   (lambda (node state)
+     (cond ((not (sccable-index node))
+            (dfs node state))
+           ((not (sccable-component node))
+            (let ((i (sccable-index node)))
+              (struct-copy
+               scca-state
+               state
+               (P (dropf (scca-state-P state) (lambda (n) (> (sccable-index n) i)))))))
+           (else
+            state)))
+   state
+   node))
 
 (define (maybe-pop-component node state)
   (match state
-    ((struct* scc-state ((S S) (P P) (component-table component-table)))
+    ((struct* scca-state ((S S) (P P)))
      (if (eq? node (car P))
          (let-values (((component S) (pop-component node S)))
-           (struct-copy
-            scc-state state
-            (S S)
-            (P (cdr P))
-            (component-table
-             (foldl
-              (lambda (node component-table)
-                (hash-set component-table node component))
-              component-table
-              component))))
+           (for-each
+            (lambda (node) (set-sccable-component! node component))
+            component)
+           (struct-copy scca-state state (S S) (P (cdr P))))
          state))))
 
 (define (pop-component node stack)
@@ -104,48 +93,54 @@
   (require racket/set)
   (require (only-in srfi/43 vector-fold))
 
-  ;; define an sccable graph node:
-  (struct node (label edges)
+  (struct node (label edges index component)
           #:mutable
           #:transparent
           #:methods gen:sccable
-          ((define (sccable-fold-edges fn state sccable)
+          ((define (scca-label sccable) (node-label sccable))
+           (define (sccable-index sccable)
+             (node-index sccable))
+           (define (set-sccable-index! sccable index)
+             (set-node-index! sccable index))
+           (define (sccable-fold-edges fn state sccable)
              (vector-fold (lambda (i state element) (fn element state))
                           state
-                          (node-edges sccable)))))
+                          (node-edges sccable)))
+           (define (sccable-component sccable)
+             (node-component sccable))
+           (define (set-sccable-component! sccable component)
+             (set-node-component! sccable component))))
 
-  ;; define the test graph:
-  (define-values (A B C D E F G H I)
-    (shared ((A (node 'A (vector B F)))
-             (B (node 'B (vector D)))
-             (C (node 'C (vector G)))
-             (D (node 'D (vector A G)))
-             (E (node 'E (vector F C)))
-             (F (node 'F (vector E)))
-             (G (node 'G (vector H)))
-             (H (node 'H (vector C I)))
-             (I (node 'I (vector))))
-      (values A B C D E F G H I)))
+  (shared ((A (node 'A (vector B F) #f #f))
+           (B (node 'B (vector D) #f #f))
+           (C (node 'C (vector G) #f #f))
+           (D (node 'D (vector A G) #f #f))
+           (E (node 'E (vector F C) #f #f))
+           (F (node 'F (vector E) #f #f))
+           (G (node 'G (vector H) #f #f))
+           (H (node 'H (vector C I) #f #f))
+           (I (node 'I (vector) #f #f)))
 
-  ;; compute the components:
-  (define component-table (scc-state-component-table (scc A)))
+          ;; compute the components:
+          (scc A)
 
-  ;; print the components:
-  (for-each
-   (lambda (node)
-     (printf "node: ~s component: ~s\n"
-             (node-label node)
-             (map node-label (hash-ref component-table node))))
-   (list A B C D E F G H I))
+          ;; print the components:
+          (for-each
+           (lambda (node)
+             (printf "node: ~s component: ~s\n"
+                     (node-label node)
+                     (map node-label (sccable-component node))))
+           (list A B C D E F G H I))
 
-  ;; run the tests:
-  (check set=? (seteq A B D) (list->seteq (hash-ref component-table A)))
-  (check set=? (seteq A B D) (list->seteq (hash-ref component-table B)))
-  (check set=? (seteq G H C) (list->seteq (hash-ref component-table C)))
-  (check set=? (seteq A B D) (list->seteq (hash-ref component-table D)))
-  (check set=? (seteq E F) (list->seteq (hash-ref component-table E)))
-  (check set=? (seteq E F) (list->seteq (hash-ref component-table F)))
-  (check set=? (seteq G H C) (list->seteq (hash-ref component-table G)))
-  (check set=? (seteq G H C) (list->seteq (hash-ref component-table H)))
-  (check set=? (seteq I) (list->seteq (hash-ref component-table I)))
-)
+          ;; run the tests:
+          (check set=? (seteq A B D) (list->seteq (sccable-component A)))
+          (check set=? (seteq A B D) (list->seteq (sccable-component B)))
+          (check set=? (seteq G H C) (list->seteq (sccable-component C)))
+          (check set=? (seteq A B D) (list->seteq (sccable-component D)))
+          (check set=? (seteq E F) (list->seteq (sccable-component E)))
+          (check set=? (seteq E F) (list->seteq (sccable-component F)))
+          (check set=? (seteq G H C) (list->seteq (sccable-component G)))
+          (check set=? (seteq G H C) (list->seteq (sccable-component H)))
+          (check set=? (seteq I) (list->seteq (sccable-component I)))
+          )
+  )
